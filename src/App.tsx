a5d9771eac3b0d7230b7013eafc19e864d73eaa6
Andrey Lavryonov <kedr_club@mail.ru>
2023-01-18 23:27:58 +0300

    feat: adds functionality and styles for states

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import styles from './App.module.css';
 import Card from './components/Card/Card';
+import { variantArray } from './const/const';
 
 function App() {
   return (
     <main className={styles.app}>
       <h1 className={styles.app__title}>Ты сегодня покормил кота?</h1>
       <ul className={styles.app__list}>
-        <li className={styles.app__item}>
-          <Card />
-        </li>
-        <li className={styles.app__item}>
-          <Card />
-        </li>
-        <li className={styles.app__item}>
-          <Card />
-        </li>
+        {variantArray.map((el) => {
+          return (
+            <li className={styles.app__item} key={el.taste}>
+              <Card
+                taste={el.taste}
+                description={el.description}
+                sign={el.sign}
+                weight={el.weight}
+                outOfStock={el.outOfStock}
+              />
+            </li>
+          );
+        })}
       </ul>
     </main>
   );
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,30 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { VariantArrayType } from '../../types/types';
 import styles from './Card.module.css';
 
-function Card() {
+function Card(props: VariantArrayType) {
+  const [isSelected, setIsSelected] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const onClick = () => {
+    if (!props.outOfStock) {
+      setIsSelected(!isSelected);
+    }
+  };
+
+  const mouseEnter = () => {
+    const current = isSelected;
+    if (current) {
+      setIsHovered(true);
+    }
+  };
+
+  const mouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <>
-      <section className={styles.card}>
+      <section
+        className={props.outOfStock ? styles.card_disabled : styles.card}
+        onClick={onClick}
+        onMouseEnter={mouseEnter}
+        onMouseLeave={mouseLeave}
+      >
         <div className={styles.card__top}>
-          <div className={styles.card__corner}></div>
-          <div className={styles.card__slogan_wrapper}>
-            <p className={styles.card__slogan}>Сказочное заморское яство</p>
+          <div
+            className={
+              isSelected && !isHovered
+                ? styles.card__corner_selected
+                : isSelected && isHovered
+                ? styles.card__corner_hovered
+                : props.outOfStock
+                ? styles.card__corner_disabled
+                : styles.card__corner
+            }
+          ></div>
+          <div
+            className={
+              isSelected && !isHovered
+                ? styles.card__slogan_wrapper_selected
+                : isSelected && isHovered
+                ? styles.card__slogan_wrapper_hovered
+                : props.outOfStock
+                ? styles.card__slogan_wrapper_disabled
+                : styles.card__slogan_wrapper
+            }
+          >
+            <p
+              className={
+                isSelected && isHovered
+                  ? styles.card__slogan_selected
+                  : props.outOfStock
+                  ? styles.card__slogan_disabled
+                  : styles.card__slogan
+              }
+            >
+              {isSelected && isHovered ? 'Котэ не одобряет?' : 'Сказочное заморское яство'}
+            </p>
           </div>
         </div>
-        <div className={styles.card__bottom}>
-          <h2 className={styles.card__title}>Нямушка</h2>
-          <p className={styles.card__taste}>с фуа-гра</p>
-          <p className={styles.card__description}>10 порций{'\n'}мышь в подарок</p>
-          <div className={styles.card__oval}>
-            <span className={styles.card__oval_weight}>0,5</span>
+        <div
+          className={
+            isSelected && !isHovered
+              ? styles.card__bottom_selected
+              : isSelected && isHovered
+              ? styles.card__bottom_hovered
+              : props.outOfStock
+              ? styles.card__bottom_disabled
+              : styles.card__bottom
+          }
+        >
+          <h2 className={props.outOfStock ? styles.card__title_disabled : styles.card__title}>
+            Нямушка
+          </h2>
+          <p className={props.outOfStock ? styles.card__taste_disabled : styles.card__taste}>
+            {props.taste}
+          </p>
+          <p
+            className={
+              props.outOfStock ? styles.card__description_disabled : styles.card__description
+            }
+          >
+            {props.description}
+          </p>
+          <div
+            className={
+              isSelected && !isHovered
+                ? styles.card__oval_selected
+                : isSelected && isHovered
+                ? styles.card__oval_hovered
+                : props.outOfStock
+                ? styles.card__oval_disabled
+                : styles.card__oval
+            }
+          >
+            <span className={styles.card__oval_weight}>{props.weight}</span>
             <span className={styles.card__oval_kg}>кг</span>
           </div>
         </div>
       </section>
-      <div className={styles.card__sign}>
-        <span className={styles.card__sign_text}>Чего сидишь? Порадуй котэ, </span>
-        <span className={styles.card__sign_link}>купи</span>
-      </div>
+      {isSelected ? (
+        <div className={styles.card__sign}>{props.sign}</div>
+      ) : props.outOfStock ? (
+        <div className={styles.card__sign_disabled}>{`Печалька, ${props.taste} закончился.`}</div>
+      ) : (
+        <div className={styles.card__sign}>
+          <span className={styles.card__sign_text}>Чего сидишь? Порадуй котэ, </span>
+          <span className={styles.card__sign_link} onClick={onClick}>
+            купи.
+          </span>
+        </div>
+      )}
     </>
   );
 }
